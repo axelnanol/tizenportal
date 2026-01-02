79ab6f24158150562471e8d058ee05f46f8064fe
Alex Nolan <alxnolan@hotmail.com>
2026-01-02 11:23:47 +0000

    v0.5.44: Hide script templates with CSS - old Tizen browsers render them as visible text

--- dist/userScript.js
+++ dist/userScript.js
@@ -682,7 +682,7 @@
         BlueMenu.init();
         Input.init(); 
         if(loaded && applied) { 
-            UI.toast("TizenPortal 0543 - Ready"); 
+            UI.toast("TizenPortal 0544 - Ready"); 
         } else if(loaded && !applied) {
             UI.toast("Config Loaded - Apply Failed");
             tpHud('Payload loaded but apply failed');
